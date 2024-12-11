5c8dd9a0331e9c8ff5e3b6bb689f06699c64ec00
mugisha isaie <mugishaisaieog1@gmail.com>
2024-12-11 17:48:57 +0200

    testing API using thunder client

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 import { useGlobalContext } from './AppContext'
 function SearchForm() {
     // const {greetings} = useGlobalContext();
+    const handleSubmit =(e)=>{
+      e.preventDefault();
+      // console.log(e.target.elements)
+      const searchValue= e.target.elements.search.value;
+      if(!searchValue) return;
+      console.log(searchValue)
+    }
   return (
-    <h2>
-      Search form
-    </h2>
+    <section>
+      <h2 className='title'>Unsplash Images</h2>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <input type="text" name='search' className='form-input search-input' placeholder='dog' />
+        <button type='submit' className='btn'>Search</button>
+      </form>
+    </section>
   )
 }
 
